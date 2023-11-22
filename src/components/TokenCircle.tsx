48877a4ed65af278696bd87082db3f9a6dc20ac6
guilemes-freitas <guilemes300@gmail.com>
2023-11-22 11:08:28 -0300

    feat: initialize responsiveness design

--- src/components/TokenCircle.tsx
+++ src/components/TokenCircle.tsx
@@ -13,12 +13,12 @@ export default function TokenCircle({token, tokenColor}: Props) {
   }
 
   return (
-    <div className={'rounded-full bg-gradient-to-b ' + `${tokenColor}` + ' w-36 h-36 flex justify-center items-center shadow-outer-circle'}>
-    <div className='rounded-full bg-white w-28 h-28 flex justify-center items-center shadow-inner-circle	'>
+    <div className={'rounded-full bg-gradient-to-b ' + `${tokenColor}` + ' w-[6.5rem] h-[6.5rem] sm:w-36 sm:h-36 flex justify-center items-center shadow-outer-circle'}>
+    <div className='rounded-full bg-white w-20 h-20 sm:w-28 sm:h-28 flex justify-center items-center shadow-inner-circle	'>
       <Image
-        className="relative text-points"
-        src={"/images/icon-"+`${token}`+".svg"}
-        alt="Scissors Image"
+        className='relative text-points max-sm:w-8 max-sm:max-h-12'
+        src={'/images/icon-'+`${token}`+'.svg'}
+        alt='Scissors Image'
         width={48}
         height={48}
         priority
