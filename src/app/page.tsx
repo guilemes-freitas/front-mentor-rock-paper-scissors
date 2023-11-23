27a7f48a454352b746c436a8e4937a5a4bdd0239
guilemes-freitas <guilemes300@gmail.com>
2023-11-23 10:31:08 -0300

    feat: finished base mobile and started base desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,43 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-8 font-barlow font-semibold'>
+    <main className='flex min-h-screen flex-col items-center gap-10 sm:justify-between pt-8 sm:p-8 font-barlow font-semibold'>
       <header className='border-2 border-header-outline rounded-lg max-w-[85vw] w-[650px] flex justify-between flex-row p-2 sm:p-4 uppercase'>
         <div className='max-sm:text-xs text-white font-bold space-x-0 flex flex-col justify-center ml-2'>
-          <p className='h-2.5 sm:h-3.5 leading-3'>Rock</p>
-          <p className='h-2.5 sm:h-3.5 leading-3'>Paper</p>
-          <p className='h-2.5 sm:h-3.5 leading-3'>Scissors</p>
-          <p className='h-2.5 sm:h-3.5 leading-3'>Lizard</p>
-          <p className='h-2.5 sm:h-3.5 leading-3'>Spock</p>
+          <p className='h-2.5 sm:h-[1.35rem] sm:text-2xl leading-3'>Rock</p>
+          <p className='h-2.5 sm:h-[1.35rem] sm:text-2xl leading-3'>Paper</p>
+          <p className='h-2.5 sm:h-[1.35rem] sm:text-2xl leading-3'>Scissors</p>
+          <p className='h-2.5 sm:h-[1.35rem] sm:text-2xl leading-3'>Lizard</p>
+          <p className='h-2.5 sm:h-[1.35rem] sm:text-2xl leading-3'>Spock</p>
         </div>
-        <div className='bg-white rounded-md text-[hsl(229, 25%, 31%)] text-center min-h-full w-20 sm:w-24 shadow-sm p-2'>
+        <div className='bg-white rounded-md text-[hsl(229, 25%, 31%)] text-center min-h-full w-20 sm:w-32 shadow-sm p-2'>
           <p className='text-score text-xs sm:text-sm tracking-widest'>Score</p>
-          <h2 className='text-points text-4xl sm:text-5xl font-bold'>12</h2>
+          <h2 className='text-points text-4xl sm:text-6xl font-bold'>12</h2>
         </div>
       </header>
 
       <section className='h-[22rem] sm:h-[36rem] flex flex-col justify-center relative'>
         <Image
-          className='absolute text-points stroke-2 w-64 h-64 right-8 sm:w-[23rem] sm:h-[23rem] sm:right-20'
+          className='absolute text-points stroke-2 w-48 h-48 top-16 right-12 sm:w-[23rem] sm:h-[23rem] sm:right-20'
           src={'/images/bg-pentagon.svg'}
           alt='Pentagon Image'
           width={48}
           height={48}
         />
-        <div className='flex justify-center items-center w-80 sm:w-[32rem] h-24 z-10'>
+        <div className='flex justify-center items-center w-72 sm:w-[32rem] h-24 z-10'>
           <TokenCircle token='scissors' tokenColor='from-scissors to-scissors-end' />
         </div>
-        <div className='flex flex-row justify-between w-80 h-28 sm:w-[32rem] sm:h-52 z-10'>
+        <div className='flex flex-row justify-between w-72 h-[5.5rem] sm:w-[32rem] sm:h-52 z-10'>
           <TokenCircle token='spock' tokenColor='from-spock to-spock-end' />
           <TokenCircle token='paper' tokenColor='from-paper to-paper-end' />
         </div>
-        <div className='flex flex-row justify-center gap-6 sm:justify-around items-center w-80 sm:w-[32rem] h-36 z-10'>
+        <div className='flex flex-row justify-center gap-6 sm:justify-around items-center w-72 sm:w-[32rem] h-36 z-10'>
           <TokenCircle token='lizard' tokenColor='from-lizard to-lizard-end' />
           <TokenCircle token='rock' tokenColor='from-rock to-rock-end' />
         </div>
       </section>
 
-      <footer>
+      <footer className='sm:self-end sm:sticky sm:bottom-[30px]'>
         <div className='border border-white rounded-lg leading-5 tracking-widest w-28 h-10 text-white uppercase text-center p-2'>Rules</div>
       </footer>
     </main>
--- src/components/TokenCircle.tsx
+++ src/components/TokenCircle.tsx
@@ -13,8 +13,8 @@ export default function TokenCircle({token, tokenColor}: Props) {
   }
 
   return (
-    <div className={'rounded-full bg-gradient-to-b ' + `${tokenColor}` + ' w-[6.5rem] h-[6.5rem] sm:w-36 sm:h-36 flex justify-center items-center shadow-outer-circle'}>
-    <div className='rounded-full bg-white w-20 h-20 sm:w-28 sm:h-28 flex justify-center items-center shadow-inner-circle	'>
+    <div className={'rounded-full bg-gradient-to-b ' + `${tokenColor}` + ' w-24 h-24 sm:w-36 sm:h-36 flex justify-center items-center shadow-outer-circle'}>
+    <div className='rounded-full bg-white w-[4.5rem] h-[4.5rem] sm:w-28 sm:h-28 flex justify-center items-center shadow-inner-circle	'>
       <Image
         className='relative text-points max-sm:w-8 max-sm:max-h-12'
         src={'/images/icon-'+`${token}`+'.svg'}
