2dbe3bde77453f4efb754b74399676b127cb37f1
guilemes-freitas <guilemes300@gmail.com>
2023-11-26 08:59:13 -0300

    feat: add rules modal funcionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+import RulesModal from '@/components/RulesModal'
 import TokenCircle from '@/components/TokenCircle'
 import Image from 'next/image'
+import { useState } from 'react';
 
 export default function Home() {
+  const [rulesModalOpen, setRulesModalOpen] = useState(false);
   return (
-    <main className='flex min-h-screen flex-col items-center gap-10 sm:justify-between pt-8 sm:p-8 font-barlow font-semibold'>
-      <header className='border-2 border-header-outline rounded-lg max-w-[85vw] w-[650px] flex justify-between flex-row p-2 sm:p-4 uppercase sm:rounded-2xl'>
+    <main className='flex min-h-screen flex-col items-center gap-10 sm:justify-between font-barlow font-semibold'>
+      <header className='border-2 border-header-outline rounded-lg max-w-[85vw] w-[650px] flex justify-between flex-row p-2 mt-8 sm:p-4 uppercase sm:rounded-2xl'>
         <div className='max-sm:text-xs text-white font-bold space-x-0 flex flex-col justify-center ml-2'>
           <p className='h-2.5 leading-3 sm:h-[1.35rem] sm:text-2xl sm:leading-6 '>Rock</p>
           <p className='h-2.5 leading-3 sm:h-[1.35rem] sm:text-2xl sm:leading-6 '>Paper</p>
@@ -39,8 +43,15 @@ export default function Home() {
         </div>
       </section>
 
-      <footer className='sm:self-end sm:sticky sm:bottom-[30px]'>
-        <div className='border border-white rounded-lg leading-5 tracking-widest w-28 h-10 text-white uppercase text-center p-2'>Rules</div>
+      {rulesModalOpen && (
+        <RulesModal
+        isOpen={rulesModalOpen}
+        handleClose={() => setRulesModalOpen(!rulesModalOpen)}/>
+        
+      )}
+
+      <footer className='sm:self-end sm:sticky sm:bottom-[30px] sm:right-[30px]'>
+        <button onClick={() => setRulesModalOpen(!rulesModalOpen)} className='border border-white rounded-lg leading-5 tracking-widest w-28 h-10 text-white uppercase text-center p-2'>Rules</button>
       </footer>
     </main>
   )
--- src/components/RulesModal.tsx
+++ src/components/RulesModal.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
+import Image from 'next/image';
 import { useSearchParams } from 'next/navigation'
 import { useRef, useEffect } from 'react'
+import React from 'react'
 
 type ModalProps = {
   isOpen: boolean,
   handleClose: () => void,
 }
 
-import React from 'react'
-
-export default function RulesModal({isOpen, handleClose}: ModalProps) {
+const RulesModal = ({isOpen, handleClose}: ModalProps) => {
   useEffect(() => {
     const closeOnEscapeKey = (e: KeyboardEvent) =>
     e.key === 'Escape' ? handleClose() : null;
@@ -18,11 +18,28 @@ export default function RulesModal({isOpen, handleClose}: ModalProps) {
       document.body.removeEventListener('keydown', closeOnEscapeKey);
     };
   }, [handleClose]);
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return (): void => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
   
   return (
-    <div className='flex flex-col justify-between p-8'>
-      <h1>Rules</h1>
-      <div></div>
+    <div className='flex flex-col items-center p-16 gap-16 bg-white h-full w-full absolute z-10' onClick={handleClose}>
+      <h1 className='text-3xl uppercase text-points font-bold'>Rules</h1>
+        <Image
+          className='h-64 w-64'
+          src={'/images/image-rules-bonus.svg'}
+          alt='Pentagon Image'
+          width={48}
+          height={48}
+        />
      </div>
   )
 }
+
+export default RulesModal 
